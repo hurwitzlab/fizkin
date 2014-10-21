0230ddb6f1d256d4b159e0c4ae0c93a9bc42fa1f
Ken Youens-Clark <kyclark@gmail.com>
2014-10-21 15:10:00 -0700

    moving fast

---
+++ scripts/02-jellyfish-count.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#
+# Create k-mer suffix arrays from a directory of FASTA files
+#
+
+source ./config.sh
+
+export CWD=$PWD
+
+PROG=`basename $0 ".sh"`
+ERR_DIR="$CWD/err/$PROG"
+OUT_DIR="$CWD/out/$PROG"
+
+create_dirs "$ERR_DIR" "$OUT_DIR"
+
+if [[ ! -d "$JELLYFISH_DIR" ]]; then
+    mkdir "$JELLYFISH_DIR"
+fi
+
+echo cd $FULL_FASTA_DIR
+cd "$FULL_FASTA_DIR"
+
+i=0
+for FILE in *.fa; do
+    export FILE
+    JOB_ID=`qsub -N jellyfish -e "$ERR_DIR/$FILE" -o "$OUT_DIR/$FILE" -v FULL_FASTA_DIR,MER_SIZE,FILE,JELLYFISH_DIR $SCRIPT_DIR/jellyfish-count.sh`
+
+    i=$((i+1))
+    printf "%5d: %s %s" $i $JOB_ID $FILE
+    echo
+done
+
+echo Submitted $i jobs for you.  Have a nice day.
---
+++ scripts/workers/jellyfish-count.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#PBS -W group_list=bhurwitz
+#PBS -q windfall
+#PBS -l jobtype=serial
+#PBS -l select=1:ncpus=12:mem=10gb
+#PBS -l place=pack:shared
+#PBS -l walltime=24:00:00
+#PBS -l cput=24:00:00
+
+source /usr/share/Modules/init/bash
+
+cd $FULL_FASTA_DIR
+
+OUT_FILE=`basename "$FILE" ".fa"`
+BLOOM="$JELLYFISH_DIR/$OUT_FILE.bc"
+JF="$JELLYFISH_DIR/$OUT_FILE.jf"
+THREADS=4
+HASH_SIZE="100M"
+
+if [ -e "$BLOOM" ]; then
+    rm -f "$BLOOM";
+fi
+
+if [ -e "$OUT_FILE" ]; then
+    rm -f "$OUT_FILE";
+fi
+
+echo $JELLYFISH bc -m $MER_SIZE -s $HASH_SIZE -t $THREADS -o "$BLOOM" "$FILE"
+
+echo $JELLYFISH count -C -m $MER_SIZE -s $HASH_SIZE -t $THREADS -o $JF --bc "$BLOOM" "$FILE"
+
+$JELLYFISH bc -m $MER_SIZE -s $HASH_SIZE -t $THREADS -o "$BLOOM" "$FILE"
+
+$JELLYFISH count -C -m $MER_SIZE -s $HASH_SIZE -t $THREADS -o $JF --bc "$BLOOM" "$FILE"
+
+rm "$BLOOM"
