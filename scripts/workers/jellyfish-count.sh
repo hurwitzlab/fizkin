53066539370efc25573fc0a7d5bb88f8fa34c117
Ken Youens-Clark <kyclark@gmail.com>
2014-11-07 16:43:20 -0700

    selecting different cpus/mem, changing jellyfish count to work by kmer vs all suffixes

--- scripts/workers/jellyfish-count.sh
+++ scripts/workers/jellyfish-count.sh
@@ -3,19 +3,19 @@
 #PBS -W group_list=bhurwitz
 #PBS -q standard
 #PBS -l jobtype=serial
-#PBS -l select=1:ncpus=12:mem=10gb
+#PBS -l select=1:ncpus=4:mem=10gb
 #PBS -l place=pack:shared
 #PBS -l walltime=24:00:00
 #PBS -l cput=24:00:00
 
 source /usr/share/Modules/init/bash
 
-cd $FULL_FASTA_DIR
+cd "$FASTA_DIR"
 
 OUT_FILE=`basename "$FILE" ".fa"`
 BLOOM="$JELLYFISH_DIR/$OUT_FILE.bc"
 JF="$JELLYFISH_DIR/$OUT_FILE.jf"
-THREADS=4
+THREADS=8
 HASH_SIZE="100M"
 
 if [ -e "$BLOOM" ]; then
@@ -26,6 +26,19 @@ if [ -e "$OUT_FILE" ]; then
     rm -f "$OUT_FILE";
 fi
 
+date
+echo Making Bloom filter
+echo $JELLYFISH bc -m $MER_SIZE -s $HASH_SIZE -t $THREADS -o "$BLOOM" "$FILE"
+
 $JELLYFISH bc -m $MER_SIZE -s $HASH_SIZE -t $THREADS -o "$BLOOM" "$FILE"
 
+echo
+date
+echo Jellyfish count
+echo $JELLYFISH count -C -m $MER_SIZE -s $HASH_SIZE -t $THREADS -o $JF --bc "$BLOOM" "$FILE"
+
 $JELLYFISH count -C -m $MER_SIZE -s $HASH_SIZE -t $THREADS -o $JF --bc "$BLOOM" "$FILE"
+
+echo
+date
+echo Finished
--- scripts/workers/launch-kmerizer.sh
+++ scripts/workers/launch-kmerizer.sh
@@ -3,7 +3,7 @@
 #PBS -W group_list=bhurwitz
 #PBS -q standard
 #PBS -l jobtype=serial
-#PBS -l select=1:ncpus=12:mem=10gb
+#PBS -l select=1:ncpus=1:mem=2gb
 #PBS -l place=pack:shared
 #PBS -l walltime=24:00:00
 #PBS -l cput=24:00:00
