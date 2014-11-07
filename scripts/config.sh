d8af1963c62cb7b523a0b6ed7bf5ccce600f5bd5
Ken Youens-Clark <kyclark@gmail.com>
2014-11-07 16:39:51 -0700

    set dir settings

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-export BASE_DIR="/rsgrps2/mbsulli/kyclark/projects/pov"
+export BASE_DIR="/rsgrps/bhurwitz/kyclark/projects/tara"
 export FASTA_DIR="$BASE_DIR/data/fasta"
-export FULL_FASTA_DIR="$BASE_DIR/data/full_fasta"
 export SUFFIX_DIR="$BASE_DIR/data/suffix"
 export KMER_DIR="$BASE_DIR/data/kmer"
 export JELLYFISH_DIR="$BASE_DIR/data/jellyfish"
 export SCRIPT_DIR="$BASE_DIR/scripts/workers"
-export BIN_DIR="$BASE_DIR/bin"
 export COUNT_DIR="$BASE_DIR/data/counts"
 export MER_SIZE=20
-export GT=/rsgrps1/mbsulli/bioinfo/biotools/bin/gt
+export BIN_DIR="/rsgrps/bhurwitz/bin"
 export JELLYFISH="$BIN_DIR/jellyfish"
 
 function create_dirs {
