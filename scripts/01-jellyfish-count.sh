0f97a4c8dbb18581fc95dce5eac55976bb7c6ca6
Ken Youens-Clark <kyclark@gmail.com>
2015-03-06 14:57:43 -0700

    now a job array

--- scripts/01-jellyfish-count.sh
+++ scripts/01-jellyfish-count.sh
@@ -8,13 +8,11 @@ source ./config.sh
 
 export CWD=$PWD
 
-PROG=`basename $0 ".sh"`
-STDERR_DIR="$CWD/err/$PROG"
-STDOUT_DIR="$CWD/out/$PROG"
+PROG=`basename "$0" ".sh"`
+ERR_DIR="$CWD/err/$PROG"
+OUT_DIR="$CWD/out/$PROG"
 
-create_dirs "$STDERR_DIR" "$STDOUT_DIR"
-
-#cd "$FASTA_DIR"
+create_dirs "$ERR_DIR" "$OUT_DIR"
 
 export SOURCE_DIR=$HOST_DIR
 export OUT_DIR=$HOST_JELLYFISH_DIR
@@ -23,22 +21,22 @@ if [[ ! -d "$OUT_DIR" ]]; then
     mkdir "$OUT_DIR"
 fi
 
-
 cd "$SOURCE_DIR"
-COUNT=`find -maxdepth 1 -type f -name \*.fa | wc -l`
+
+export FILES_LIST="$SOURCE_DIR/files-list"
+
+find -maxdepth 1 -type f -name \*.fa > $FILES_LIST
+
+COUNT=`wc -l $FILES_LIST | cut -d ' ' -f 1`
+
 echo Found $COUNT files in \"$SOURCE_DIR\"
 
-i=0
-for FASTA in *.fa; do
-    i=$((i+1))
+if [ $COUNT -gt 0 ]; then
+    JOB_ID=`qsub -N jellyfish -e "$ERR_DIR/$FASTA" -o "$OUT_DIR/$FASTA" \
+        -v SOURCE_DIR,MER_SIZE,FILES_LIST,JELLYFISH,OUT_DIR -J 1-$COUNT \
+        $SCRIPT_DIR/jellyfish-count.sh`
 
-    export FILE=`readlink -f $FASTA`
-
-    JOB_ID=`qsub -N jellyfish -e "$STDERR_DIR/$FASTA" -o "$STDOUT_DIR/$FASTA" -v SOURCE_DIR,MER_SIZE,FILE,JELLYFISH,OUT_DIR $SCRIPT_DIR/jellyfish-count.sh`
-
-    $QSTAT -f $JOB_ID > "$STDOUT_DIR/$FASTA"
-
-    printf "%5d: %s %s\n" $i $JOB_ID $FASTA
-done
-
-echo Submitted $i jobs for you.  Have a nice day.
+    echo Job ID: \"$JOB_ID\"
+else
+    echo Nothing to do!
+fi
