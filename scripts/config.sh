f9168182f8f8e0ea3de0a06e68cff39449f67a67
Ken Youens-Clark <kyclark@gmail.com>
2015-03-30 16:16:44 -0700

    added comments

--- scripts/config.sh
+++ scripts/config.sh
@@ -8,14 +8,48 @@
 #
 # --------------------------------------------------
 
-export SCRIPT_DIR="/rsgrps/bhurwitz/kyclark/mouse/scripts/workers"
+#
+# Some constants
+#
+export MER_SIZE=20
+export QSTAT="/usr/local/bin/qstat_local"
+export GUNZIP="/bin/gunzip"
+
+#
+# The main checkout
+#
+PROJECT_DIR="/rsgrps/bhurwitz/kyclark/mouse/"
+
+#
+# Where we can find the worker scripts
+#
+export SCRIPT_DIR="$PROJECT_DIR/scripts/workers"
+
+#
+# Where to put all our generated data
+#
+export DATA_DIR="$PROJECT_DIR/data"
+
+# 
+# Where we can find all our custom binaries (e.g., jellyfish)
+# 
 export BIN_DIR="/rsgrps/bhurwitz/hurwitzlab/bin"
 export JELLYFISH="$BIN_DIR/jellyfish"
-export RAW_DIR="/rsgrps/bhurwitz/hurwitzlab/data/raw/Doetschman_20111007/all"
-export HOST_DIR="/rsgrps/bhurwitz/hurwitzlab/data/reference/mouse_genome/20141111"
-export HOST_JELLYFISH_DIR="/rsgrps/bhurwitz/hurwitzlab/data/jellyfish/mouse"
 
-export DATA_DIR="/rsgrps/bhurwitz/kyclark/mouse/data"
+#
+# Where to find the "raw" DNA reads
+#
+export RAW_DIR="/rsgrps/bhurwitz/hurwitzlab/data/raw/Doetschman_20111007/all"
+
+#
+# Where to find the host genome for screening
+#
+export HOST_DIR="/rsgrps/bhurwitz/hurwitzlab/data/reference/mouse_genome/20141111"
+
+#
+# Where to put the results of our steps
+#
+export HOST_JELLYFISH_DIR="$DATA_DIR/jellyfish/mouse"
 export FASTQ_DIR="$DATA_DIR/fastq"
 export FASTA_DIR="$DATA_DIR/fasta"
 export SCREENED_DIR="$DATA_DIR/screened"
@@ -24,10 +58,10 @@ export KMER_DIR="$DATA_DIR/kmer"
 export JELLYFISH_DIR="$DATA_DIR/jellyfish"
 export COUNT_DIR="$DATA_DIR/counts"
 export MODE_DIR="$DATA_DIR/modes"
-export MER_SIZE=20
-export QSTAT="/usr/local/bin/qstat_local"
-export GUNZIP="/bin/gunzip"
 
+#
+# Some custom functions for our scripts
+#
 # --------------------------------------------------
 function init_dirs {
     for dir in $*; do
