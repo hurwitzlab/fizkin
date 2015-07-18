36266f42528a2182412dc05e8b60680754b15df6
Ken Youens-Clark <kyclark@gmail.com>
2015-07-18 11:54:49 -0700

    added qsub args

--- scripts/05-make-matrix.sh
+++ scripts/05-make-matrix.sh
@@ -19,7 +19,14 @@ STDOUT_DIR="$CWD/out/$PROG"
 
 init_dirs "$STDOUT_DIR" "$MATRIX_DIR"
 
-JOB=$(qsub -N "mk-matrix" -j oe -o "$STDOUT_DIR" -v SCRIPT_DIR,MODE_DIR,MATRIX_DIR $SCRIPT_DIR/make-matrix.sh)
+EMAIL_ARG=""
+if [[ ! -z $EMAIL ]]; then
+  EMAIL_ARG="-M $EMAIL -m ea"
+fi
+
+GROUP_ARG="-W group_list=${GROUP:=bhurwitz}"
+
+JOB=$(qsub -N "mk-matrix" -j oe -o "$STDOUT_DIR" $EMAIL_ARG $GROUP_ARG -v SCRIPT_DIR,MODE_DIR,MATRIX_DIR $SCRIPT_DIR/make-matrix.sh)
 
 if [ $? -eq 0 ]; then
   echo Submitted job \"$JOB.\" Do svidaniya.
