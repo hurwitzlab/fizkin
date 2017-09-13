54c926e04e9a8906ea70465bbf569bc1cbcb0baa
Ken Youens-Clark <kyclark@gmail.com>
2017-09-13 16:30:25 -0700

    merging everything here

---
+++ scripts/install.R
@@ -0,0 +1,16 @@
+#!/usr/bin/env Rscript
+
+install.packages("curl")
+install.packages("openssl")
+install.packages("devtools")
+install.packages("optparse")
+install.packages("plyr")
+install.packages("ggplot2")
+install.packages("R.utils")
+install.packages("xtable")
+install.packages("vegan")
+install.packages("hash")
+install.packages("devtools")
+library(devtools)
+install_github("igraph/rigraph")
+install.packages("networkD3")
