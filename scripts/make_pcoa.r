1116eac8bd6ab3d97703b6446b4b80d5f82ccde7
Ken Youens-Clark <kyclark@gmail.com>
2017-11-30 09:19:26 -0700

    embettered

--- scripts/make_pcoa.r
+++ scripts/make_pcoa.r
@@ -1,62 +1,101 @@
 #!/usr/bin/env Rscript
 
 library("optparse")
+library("vegan")
 
 # set arguments
-option_list <- list (
-    make_option(c("-d","--dir"), type = "character", default = "$HOME",
-                help = "set work directory [default= %default]"),  
-    make_option(c("-f","--file"), type = "character", default=NULL,
-                help="data file name", metavar="character"),
-    make_option(c("-o","--out"), type = "character", default="out",
-                help="output file name [default= %default]"),
-    make_option(c("-n","--number"), type = "integer", default=NULL,
-                help="total number of reads per sample"),
-    make_option(c("-t","--title"), type = "character", default=NULL,
-                help="title of PCoA plot [default= %default]")
+option_list = list (
+    make_option(c("-d", "--dir"), 
+                type = "character", 
+                default = getwd(),
+                help = "set work directory (%default)"
+                ),  
+    make_option(c("-f", "--file"), 
+                type = "character", 
+                default = "",
+                help = "Input file", 
+                metavar="character"
+                ),
+    make_option(c("-o", "--out"), 
+                type = "character", 
+                default = "pcoa.pdf",
+                help="output file name (%default)"
+                ),
+    make_option(c("-n", "--number"), 
+                type = "integer", 
+                default = 1,
+                help = "Total number of reads per sample (%default)"
+                ),
+    make_option(c("-t", "--title"), 
+                type = "character", 
+                default = "PCOA",
+                help = "Title of PCoA plot (%default)"
+                )
 );
 
-opt_parser <- OptionParser(option_list = option_list)
-opt <- parse_args(opt_parser)
+opt_parser = OptionParser(option_list = option_list)
+opt        = parse_args(opt_parser)
+out_dir    = opt$dir
+infile     = opt$file
+nreads     = opt$number
+out_file   = opt$out
+title      = opt$title
 
-# check arguments
-if (is.null(opt$file) | is.null(opt$number)) {
-  print_help(opt_parser)
-  stop("data file or total number of reads are missing", call.=FALSE)
+if (!dir.exists(out_dir)) {
+    printf("Creating outdir '%s'\n", out_dir)
+    dir.create(out_dir)
 }
 
-# make PCoA
-library(vegan)
-setwd(opt$dir)
+setwd(out_dir)
+
+# check arguments
+if (nchar(infile) == 0) {
+    stop("Missing --file")
+}
+
+if (nreads < 1) {
+    stop("--number (of reads) must be a positive integer")
+}
 
 # input fizkin matrix
-fiz <- as.data.frame(read.table(opt$file,header=TRUE,
-                                sep = ",",row.names = 1))
-colnames(fiz) <- row.names(fiz)
+fiz = as.data.frame(read.table(infile, header = TRUE))
+#fiz = as.data.frame(read.table(infile, sep = ',', row.names = 1, header = TRUE))
+#colnames(fiz) = row.names(fiz)
+
+#print(fiz)
 
 # scaling to mash range (0 to 1)
-fiz <- fiz/opt$number
+fiz = fiz/nreads
 
 # make euclidean distance matrix 
-fiz_dis <- as.data.frame(as.matrix(dist(fiz,method="euclidean")))
+fiz_dis = as.data.frame(as.matrix(dist(fiz, method = "euclidean")))
 
 # calculate PCoA 
-fiz_pcoa <- rda(fiz_dis[1:9])
+fiz_pcoa = rda(fiz_dis)
 
 # calculating PCoA1% and PCoA2%
-pcoa1_number <- round(fiz_pcoa$CA$eig[1]/sum(fiz_pcoa$CA$eig)*100,digits = 2)
-pcoa2_number <- round(fiz_pcoa$CA$eig[2]/sum(fiz_pcoa$CA$eig)*100,digits = 2)
+pcoa1_number = round(fiz_pcoa$CA$eig[1]/sum(fiz_pcoa$CA$eig)*100, digits = 2)
+pcoa2_number = round(fiz_pcoa$CA$eig[2]/sum(fiz_pcoa$CA$eig)*100, digits = 2)
 
 # make x-y label name
-xlabel <- paste("PCoA1",paste('(',pcoa1_number,'%',')', sep=''))
-ylabel <- paste("PCoA2",paste('(',pcoa2_number,'%',')', sep=''))
+xlabel = paste("PCoA1", paste('(', pcoa1_number, '%', ')', sep=''))
+ylabel = paste("PCoA2", paste('(', pcoa2_number, '%', ')', sep=''))
 
 # plot PCoA
-setEPS()
-postscript(opt$out)
-biplot(fiz_pcoa,display = "sites",col="red",cex=2,
-       xlab= paste(xlabel),
-       ylab= paste(ylabel))
-points(fiz_pcoa,display = "sites",cex =1.5,pch=20,col="red")
-title(main = opt$title)
+pdf(out_file, width = 6, height = 6)
+biplot(fiz_pcoa,
+       display = "sites",
+       col     = "black",
+       cex     = 2,
+       xlab    = paste(xlabel),
+       ylab    = paste(ylabel)
+       )
+points(fiz_pcoa,
+       display = "sites",
+       col     = "black",
+       cex     = .5,
+       pch     = 20
+       )
+title(main = title)
 dev.off()
+print(paste("Done, see", out_file))
