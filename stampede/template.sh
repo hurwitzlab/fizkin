1a915472f0ecedcc39a71b30acef46cf37109262
Ken Youens-Clark <kyclark@gmail.com>
2018-04-03 11:33:57 -0700

    updated with new options

--- stampede/template.sh
+++ stampede/template.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo "QUERY            \"${QUERY}\""
-echo "ALIAS_FILE       \"${ALIAS_FILE}\""
-echo "EUC_DIST_PERCENT \"${EUC_DIST_PERCENT}\""
-echo "SAMPLE_DIST      \"${SAMPLE_DIST}\""
-echo "NUM_SCANS        \"${NUM_SCANS}\""
-echo "METADATA_FILE    \"${METADATA_FILE}\""
+echo "QUERY                  \"${QUERY}\""
+echo "ALIAS_FILE             \"${ALIAS_FILE}\""
+echo "EUC_DIST_PERCENT       \"${EUC_DIST_PERCENT}\""
+echo "SAMPLE_DIST            \"${SAMPLE_DIST}\""
+echo "NUM_SCANS              \"${NUM_SCANS}\""
+echo "METADATA_FILE          \"${METADATA_FILE}\""
+echo "PCT_KMER_READ_COVERAGE \"${PCT_KMER_READ_COVERAGE}\"
 
-sh run.sh ${QUERY} ${ALIAS_FILE} ${EUC_DIST_PERCENT} ${SAMPLE_DIST} ${NUM_SCANS} ${METADATA_FILE} ${MIN_NUM_KMERS_FOR_MODE}
+sh run.sh ${QUERY} ${ALIAS_FILE} ${EUC_DIST_PERCENT} ${SAMPLE_DIST} ${NUM_SCANS} ${METADATA_FILE} ${PCT_KMER_READ_COVERAGE}
